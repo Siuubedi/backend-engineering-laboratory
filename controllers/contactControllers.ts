7c43362467efceb79d3e53a5168fac43b1980863
subigya-js <developer.subigya@gmail.com>
2026-08-10 11:17:44 +0545

    feat: validate createContact input and update dev script to use tsx

--- controllers/contactControllers.ts
+++ controllers/contactControllers.ts
@@ -29,6 +29,13 @@ export const getContactById = (req: Request, res: Response) => {
 // @route POST /api/contacts
 // @access Public
 export const createContact = (req: Request, res: Response) => {
+    const { name, email, contact } = req.body
+    if (!name || !email || !contact) {
+        res.status(400).json({ message: "Please provide all the fields" })
+        console.log("Please provide all the fields")
+        return
+    }
+
     console.log("Contact created:", req.body)
     res.status(201).json({ message: "Contact created!" })
 }
